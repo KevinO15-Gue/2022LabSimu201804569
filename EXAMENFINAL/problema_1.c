d6f21d854ebacae8c0e53f680bac82df698b4999
KevinO15-Gue <kevbb15399@gmail.com>
2022-05-16 08:19:59 -0600

    MAY16

---
+++ EXAMENFINAL/problema_1.c
@@ -0,0 +1,14 @@
+/*
+Autor:          Kevin Orellana
+Carnet:         201804569
+compilador:     gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0
+compilar:       gcc -o problema_1.out problema_1.c 
+Fecha:          16 de mayo de 2022
+Librerías:      stdio, stdlib
+Resumen:        Se resuelve el problema 1 del EXAMENFINAL del Laboratorio de Simulación donde se solicita
+                crear un programa que simule el despeje de cohetes.
+*/
+
+//librerias
+#include <stdio.h>
+#include <stdlib.h>
